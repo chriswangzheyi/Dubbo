8699aff1dcc31d2e4a0328f3e97faa6a8a766c4c
zheyi <chriswangzheyi@hotmail.com>
2018-03-01 17:06:50 +0800

    增加dubbo_web
    增加dubbo_web

---
+++ Dubbo-Web/src/main/java/com/wzy/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.wzy.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wzy.api.IMessage.IMessage;
+
+
+
+@Controller 
+public class MessageController {
+	@Resource
+	private IMessage message;
+	
+	
+	@RequestMapping("/echo")
+	public ModelAndView echo(String msg) {
+		System.out.println("111111111");
+		ModelAndView mav = new ModelAndView("messages/show") ;
+		mav.addObject("info", this.message.echo(msg)) ;
+		return mav ; 
+	}
+}
